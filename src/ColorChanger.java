fbaaffd34f121f2bfd05b9234a66f747954dfd27
npjelinski <npjelinski@gmail.com>
2017-08-18 11:00:09 -0400

    Added feature to show only red color

--- src/ColorChanger.java
+++ src/ColorChanger.java
@@ -10,14 +10,16 @@ public class ColorChanger
 	static BufferedImage img; // The raw image data
 	
 	static int[] rgbarray; // The 1D array of pixels
+	static int[] newimgarray;
 	public static Color currentColor; // Wrapper for int color value of pixel
 	public static Color newColor; // new color to convert back to RGB values
 	
 	public static void main(String[] args) 
 	{
+		Scanner scan = new Scanner(System.in);
 		try
 		{
-			img = ImageIO.read(new File(new String(args[0])));
+			img = ImageIO.read(new File("rose.jpg"));
 		}
 		catch (IOException e) 
 		{
@@ -29,32 +31,71 @@ public class ColorChanger
 			
 		rgbarray = img.getRGB(0, 0, width, height, null, 0, width);
 		
-		int[] newimgarray = new int[rgbarray.length];
-		
-		for (int i = 0; i < rgbarray.length; i++)
-		{
-			currentColor = new Color(rgbarray[i]);
-			
-			int newRed = (int) (.2989 * currentColor.getRed()); // take apart RGB value, apply formula
-			int newGreen = (int) (.587 * currentColor.getGreen());
-			int newBlue = (int) (.114 * currentColor.getBlue());
-			
-			int newShade = newRed + newGreen + newBlue; // grayscale value
-			newColor = new Color(newShade, newShade, newShade);
-			newimgarray[i] = newColor.getRGB(); // pull RGB value
-		}
+		newimgarray = new int[rgbarray.length];
 		
 		BufferedImage newimg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-		newimg.setRGB(0, 0, width, height, newimgarray, 0, width); // re-create image
+		
+		System.out.println("Enter 1 for Grayscale, 2 for Monocolor Red.");
+		int selection = scan.nextInt();
+		
+		switch(selection)
+		{
+			case 1: grayFilter();
+					break;
+			case 2: monocolorRed();
+					break;
+		}
 		
 		try
 		{
-			File outputfile = new File(new String(args[1]));
+			newimg.setRGB(0, 0, width, height, newimgarray, 0, width); // re-create image
+			File outputfile = new File("rose-red.jpg");
 			ImageIO.write(newimg, "jpg", outputfile);
+			System.out.println("Here");
 		} 
 		catch(IOException e) 
 		{
 			System.out.println("Cannot write file.");
+		}
+	}
+	
+	public static void grayFilter()
+	{
+		int newRed, newGreen, newBlue, newShade;
+		for (int i = 0; i < rgbarray.length; i++)
+		{
+			currentColor = new Color(rgbarray[i]);
+			
+			newRed = (int) (.2989 * currentColor.getRed()); // take apart RGB value, apply formula
+			newGreen = (int) (.587 * currentColor.getGreen());
+			newBlue = (int) (.114 * currentColor.getBlue());
+			
+			newShade = newRed + newGreen + newBlue; // grayscale value
+			newColor = new Color(newShade, newShade, newShade);
+			newimgarray[i] = newColor.getRGB(); // pull RGB value
+		}
+	}
+	
+	public static void monocolorRed()
+	{
+		int newRed, newGreen, newBlue, newShade;
+		for (int i = 0; i < rgbarray.length; i++)
+		{
+			currentColor = new Color(rgbarray[i]);
+			if (currentColor.getRed() <= 150 || currentColor.getGreen() >= 100)
+			{
+				newRed = (int) (.2989 * currentColor.getRed()); // take apart RGB value, apply formula
+				newGreen = (int) (.587 * currentColor.getGreen());
+				newBlue = (int) (.114 * currentColor.getBlue());
+				
+				newShade = newRed + newGreen + newBlue; // grayscale value
+				newColor = new Color(newShade, newShade, newShade);
+				newimgarray[i] = newColor.getRGB(); // pull RGB value
+			}
+			else
+			{
+				newimgarray[i] = rgbarray[i];
+			}
 		}
 	}
 }
